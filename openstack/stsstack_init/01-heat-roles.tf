5c13e4d5f6ca48f035ee21a9755cfe33d7046374
Arif Ali <arif.ali@canonical.com>
2023-09-13 16:51:11 +0100

    Grab the heat admin password from file

--- openstack/stsstack_init/01-heat-roles.tf
+++ openstack/stsstack_init/01-heat-roles.tf
@@ -1,6 +1,6 @@
 resource "openstack_identity_project_v3" "heat_domain" {
     name          = "heat"
-    description   = "(tf managed) Domain for heat"
+    description   = "Stack projects and users"
     enabled       = true
     is_domain     = true
 }
@@ -9,7 +9,7 @@ resource "openstack_identity_user_v3" "heat_domain_admin" {
   domain_id = openstack_identity_project_v3.heat_domain.id
   name = "heat_domain_admin"
 
-  password = "Ht8NdKTGdpJjRsS4V33tsVW4mSztgZMs" # leader-get heat-domain-admin-passwd
+  password = file("heat-domain-admin-passwd")
 }
 
 resource "openstack_identity_role_assignment_v3" "heat_admin_role_assignment" {
